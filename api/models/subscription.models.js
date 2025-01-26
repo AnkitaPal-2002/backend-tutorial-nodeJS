ebd22f415e6b03774d7ef55937806cd661252e98
AnkitaPal-2002 <ankitapal6112002@gmail.com>
2025-01-26 22:49:06 +0530

    feat: Add subscription model

---
+++ api/models/subscription.models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber:{
+        type: mongoose.Schema.Types.ObjectId, //one who is subscribing
+        ref: 'User'
+    },
+
+    channel:{
+        type: mongoose.Schema.Types.ObjectId, //one to whom 'subscriber' is subscribing
+        ref: 'User'
+    },
+    
+
+
+    
+
+},{timestamps:true});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+module.exports = Subscription;
